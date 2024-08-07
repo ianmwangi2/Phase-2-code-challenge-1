f4ef0c69548aa20ff5edad52e63dff1c53289010
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-08-07 10:23:37 +0300

    Corrected

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -19,18 +19,12 @@ function Transactions() {
                 <th>Category</th>
                 <th>Amount</th>
             </tr>
-            {
-                transactions.map(transaction => {
-                    return (
-                        <tr>
-                            <td>{transaction.date}</td>
-                            <td>{transaction.description}</td>
-                            <td>{transaction.category}</td>
-                            <td>{transaction.amount}</td>
-                        </tr>
-                    )
-                })
-            }
+            <tr>
+                <td>{transaction.date}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.amount}</td>
+            </tr>
         </table>
     </div>
   )
